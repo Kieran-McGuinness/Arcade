3d31f3dc99b43bc137b46f164de0884d82397691
Kieran McGuinness <zumaht@gmail.com>
2021-12-14 18:40:27 -0500

    Worked on more functions of snake

--- arcade/app.js
+++ arcade/app.js
@@ -1,42 +1,116 @@
 let initialState;
-let score = 0
-let playingField = document.getElementsByTagName("tr");
+let table = document.getElementsByTagName("table")[0];
+let score = 0;
+let highScore = 0;
+let playingField = [];
 let playingFieldArray = [];
+let rowCount = 0;
+let columnCount = 0;
 let snake = {
-    body: [[6,5],[6,6],[6,7]],
-    nextDirection: [0,1],
-    nextDirectionWord: "right"
+    body: [],
+    nextDirection: [1,0],
+    nextDirectionWord: "down"
 }
-let intervalSpeed = 1300;
-// let speed = setInterval(tick, intervalSpeed);
-document.onload = createArray()
-let rowCount = playingFieldArray[0].length;
-let columnCount = document.getElementsByTagName("tr").length;
+let speed = 800;
+let movement = ""
 let randomRow = 0;
 let randomColumn = 0;
-
-
- function createArray(){
-for (let i = 0; i < playingField.length; i++){
-playingFieldArray.push(playingField[i].children)
-}
-}
-
-
-
-function renderState(){
-    for (let q = 0; q < snake.body.length; q++){
-        playingFieldArray[snake.body[q][0]][snake.body[q][1]].className = "snake"
+function createArray(){
+    for (let i = 0; i < playingField.length; i++){
+    playingFieldArray.push(playingField[i].children)
     }
 }
+// function resetGame(){
+// remove "removed" from boardsetup
+// delete created boardsetip
+// reset score
 
-function onBoardClick(){
-
-
-
-renderState()
-
+// }
+function makePlayingField(rowLength, columnLength){
+    for (let o = 0; o < columnLength; o++ ){
+    makeRow(rowLength)
+    }
+playingField = document.getElementsByTagName("tr");
+createArray();
+rowCount = playingFieldArray[0].length;
+columnCount = document.getElementsByTagName("tr").length;
+let snakeStart = Math.floor(rowCount/2)
+snake.body.push([0, snakeStart]);
+snake.body.push([0, snakeStart+1]);
+table.id = "tablestyle";
 }
+
+document.getElementById("submit").addEventListener("click", customField)
+document.getElementById("quickplay").addEventListener("click", quickPlay)
+document.getElementById("playbtn").addEventListener("click", startSnake)
+document.getElementById("resetbtn").addEventListener("click", resetGame)
+
+function resetGame(){
+    for (let p = 0; p < columnCount; p++){
+    playingField[0].remove()
+    }
+    document.getElementById("boardsetup").classList.remove("removed");
+    document.getElementById("playbtn").classList.add("removed");
+    document.getElementById("resetbtn").classList.add("removed");
+    table.id = "";
+    score = 0;
+    document.getElementById("score").innerText = score;
+    clearInterval(movement)
+    playingField = [];
+playingFieldArray = [];
+rowCount = 0;
+columnCount = 0;
+snake.body = []
+snake.nextDirection = [1,0]
+snake.nextDirectionWord = "down"
+}
+
+function startSnake(){
+   movement = setInterval(tick, speed)
+}
+
+function quickPlay(){
+    makePlayingField(17, 10)
+    document.getElementById("boardsetup").classList.add("removed")
+    document.getElementById("playbtn").classList.remove("removed")
+    document.getElementById("resetbtn").classList.remove("removed")
+    speed = 200;
+}
+function customField(){
+    let customRow = document.getElementById("rows").value;
+    let customColumn = document.getElementById("columns").value;
+    document.getElementById("boardsetup").classList.add("removed")
+    document.getElementById("playbtn").classList.remove("removed")
+    document.getElementById("resetbtn").classList.remove("removed")
+    makePlayingField(customRow, customColumn)
+    
+}
+document.getElementById("selector").addEventListener("change", function (event){
+speed = Number(event.target.value)
+})
+
+function makeRow(length){
+    let row = document.createElement("tr");
+    for (let r = 0; r < length; r++){
+        let td = document.createElement("td")
+        row.appendChild(td)
+    }
+    table.appendChild(row)
+}
+
+// function renderState(){
+//     for (let q = 0; q < snake.body.length; q++){
+//         playingFieldArray[snake.body[q][0]][snake.body[q][1]].className = "snake"
+//     }
+// }
+
+// function onBoardClick(){
+
+
+
+// renderState()
+
+// }
 
 document.addEventListener('keydown', controls)
 function controls (event){
@@ -73,7 +147,7 @@ function apple(){
         randomRow = Math.floor(Math.random() * (rowCount-1))
         randomColumn = Math.floor(Math.random() * (columnCount-1))
    if (playingFieldArray[randomColumn][randomRow].className !== "snake"){
-    playingFieldArray[randomColumn][randomRow].id = "apple"
+    playingFieldArray[randomColumn][randomRow].id = "apple";
    }else {
        apple()
    }
@@ -85,24 +159,35 @@ function apple(){
 
 function tick(){
     apple();
-    if (playingFieldArray[snake.body[snake.body.length-1][0] + snake.nextDirection[0]] [snake.body[snake.body.length-1][1] + snake.nextDirection[1]] === undefined){
-        clearInterval(speed);
-        alert("out of bounds")
-    }else if (playingFieldArray[snake.body[snake.body.length-1][0] + snake.nextDirection[0]] [snake.body[snake.body.length-1][1] + snake.nextDirection[1]].className === "snake"){
-        clearInterval(speed);
+    let nextColumn = snake.body[snake.body.length-1][0] + snake.nextDirection[0];
+    let nextRow = snake.body[snake.body.length-1][1] + snake.nextDirection[1];
+    if (nextColumn > columnCount - 1 || nextColumn < 0){
+        alert ("out of bounds")
+        clearInterval(movement)
+    }
+    else if (nextRow > rowCount - 1 || nextRow < 0){
+        alert ("out of bounds")
+        clearInterval(movement)
+    }else if (playingFieldArray[nextColumn] [nextRow].className === "snake"){
         alert("you ate yourself")
+        clearInterval(movement)
     } else {
-        if (playingFieldArray[snake.body[snake.body.length-1][0] + snake.nextDirection[0]] [snake.body[snake.body.length-1][1] + snake.nextDirection[1]].id !== "apple"){
+        if (playingFieldArray[nextColumn] [nextRow].id !== "apple"){
             playingFieldArray[snake.body[0][0]][snake.body[0][1]].className = ""
             snake.body.shift([0])
         }else {
             playingFieldArray[randomColumn][randomRow].id = ""
             score += 1;
             document.getElementById("score").innerText = score;
+                if(score > highScore){
+                    highScore = score;
+                    document.getElementById("highscore").innerText = highScore
+                }
         }
     snake.body.push([snake.body[snake.body.length-1][0] + snake.nextDirection[0], snake.body[snake.body.length-1][1] + snake.nextDirection[1]])
     
     for (let q = 0; q < snake.body.length; q++){
+    playingFieldArray[snake.body[q][0]][snake.body[q][1]].style.background = ""
     playingFieldArray[snake.body[q][0]][snake.body[q][1]].className = "snake"
  
 }
@@ -112,7 +197,3 @@ function tick(){
 
    
 }
-
-
-
-
